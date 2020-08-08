0e70fe5d78b397080d9b29187da1b6db75f5da8b
Orlando <batres.93@gmail.com>
2020-08-08 10:28:14 -0600

    Primera prueba de autenticacion por medio de SOAP y autenticacion basica

--- PracticasSA/pruebasSOAP.py
+++ PracticasSA/pruebasSOAP.py
@@ -1,6 +1,6 @@
 from zeep import Client
 from zeep import xsd
-
+import globus_sdk
 
 cliente  = Client(wsdl='https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap&wsdl')
 
@@ -10,8 +10,11 @@ print(cliente)
 header = xsd.Element(
     '{https://api.softwareavanzado.world/}auth',
     xsd.ComplexType([
-        xsd.Element(
-            '{https://api.softwareavanzado.world/}username',
-            xsd.String()),
+        xsd.Element('{https://api.softwareavanzado.world/}username',xsd.String()),
+        xsd.Element('{https://api.softwareavanzado.world/}password',xsd.String()),
     ])
 )
+
+header_value = header(username='sa', password='usac')
+print (cliente.service.readList(_soapheaders=[header_value]))
+
